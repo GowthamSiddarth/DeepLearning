ab160826f67a1d81053f69a6bf3348c2dcb15135
Gowtham Behara <gothsiddu@gmail.com>
2018-05-13 15:02:06 +0530

    Add tester function to test softmax function

--- tester/__init__.py
+++ tester/__init__.py
@@ -12,6 +12,15 @@ def test_sigmoid():
     logging.info(res)
 
 
+def test_softmax():
+    softmax_input = np.array([2, 3, 5, 6])
+
+    from helper import softmax
+    res = softmax(softmax_input)
+
+    logging.info(res)
+
+
 if __name__ == "__main__":
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format="%(asctime)s: %(levelname)s - %(message)s")
     logging.info("Started running the tester functions ...")
