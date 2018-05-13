d3633456d3e0a905bebfb1aedcb2e4a0a6819b18
Gowtham Behara <gothsiddu@gmail.com>
2018-05-13 15:11:06 +0530

    Plot sigmoid scores for each input

--- helper/__init__.py
+++ helper/__init__.py
@@ -1,7 +1,15 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def sigmoid(x): return 1 / (1 + np.exp(-x))
 
 
 def softmax(x): return np.exp(x) / np.sum(np.exp(x), axis=0)
+
+
+def line_graph(x, y, x_title, y_title):
+    plt.plot(x, y)
+    plt.xlabel(x_title)
+    plt.ylabel(y_title)
+    plt.show()
--- tester/__init__.py
+++ tester/__init__.py
@@ -3,25 +3,36 @@ import logging
 import sys
 
 
-def test_sigmoid():
-    sigmoid_input = np.array([2, 3, 5, 6])
-
+def test_sigmoid(sigmoid_input):
     from helper import sigmoid
     res = sigmoid(sigmoid_input)
 
-    logging.info(res)
+    return res
 
 
-def test_softmax():
-    softmax_input = np.array([2, 3, 5, 6])
-
+def test_softmax(softmax_input):
     from helper import softmax
     res = softmax(softmax_input)
 
-    logging.info(res)
+    return res
+
+
+def plot_sigmoid_scores(x, y, x_title="Input", y_title="Sigmoid Score"):
+    from helper import line_graph
+
+    line_graph(x, y, x_title=x_title, y_title=y_title)
 
 
 if __name__ == "__main__":
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format="%(asctime)s: %(levelname)s - %(message)s")
     logging.info("Started running the tester functions ...")
-    test_sigmoid()
+
+    sigmoid_input = np.array([2, 3, 5, 6])
+    res = test_sigmoid(sigmoid_input)
+    logging.info(res)
+
+    plot_sigmoid_scores(sigmoid_input, res)
+
+    softmax_input = np.array([2, 3, 5, 6])
+    res = test_softmax(softmax_input)
+    logging.info(res)
