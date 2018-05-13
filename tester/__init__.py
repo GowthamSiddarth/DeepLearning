edb6535d7ce4e37f6903e0564d6e380315879241
Gowtham Behara <gothsiddu@gmail.com>
2018-05-13 15:01:42 +0530

    Add tester function to test sigmoid function

---
+++ tester/__init__.py
@@ -0,0 +1,18 @@
+import numpy as np
+import logging
+import sys
+
+
+def test_sigmoid():
+    sigmoid_input = np.array([2, 3, 5, 6])
+
+    from helper import sigmoid
+    res = sigmoid(sigmoid_input)
+
+    logging.info(res)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format="%(asctime)s: %(levelname)s - %(message)s")
+    logging.info("Started running the tester functions ...")
+    test_sigmoid()
